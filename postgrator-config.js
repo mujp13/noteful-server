2117086a622490bb305b145a93992fa513ab483c
Kevin Park <keunsuk413@gmail.com>
2020-04-05 18:27:54 -0500

    deployed to prod

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL
+  "connectionString": DATABASE_URL,
+  "ssl": !!process.env.SSL
 }
